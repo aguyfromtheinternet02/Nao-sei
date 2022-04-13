ce07d1b97ce23855214dfef566d9739a113e193b
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-04-13 10:33:43 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -15,7 +15,7 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 120; // How many frames per second the game should run at.
+	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	var memoryCounter:MemoryCounter;
@@ -74,18 +74,12 @@ class Main extends Sprite
 		if(FlxG.save.data.framerateDraw >= 60 && FlxG.save.data.framerateDraw != null)
 			FlxG.drawFramerate = FlxG.save.data.framerateDraw;
 		
-		#if !mobile
-		
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
 		memoryCounter = new MemoryCounter(10, 3, 0xffffff);
 		addChild(memoryCounter);
 		toggleMem(FlxG.save.data.mem);
-
-		#end
-
-
 	}
 	var fpsCounter:FPS;
 
