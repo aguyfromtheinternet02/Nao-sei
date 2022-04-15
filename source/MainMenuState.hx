3a578bc0e878eefe899accb16f31b2a71b9a00df
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-04-15 12:30:57 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -131,7 +131,7 @@ class MainMenuState extends MusicBeatState
 		changeItem();
 
                 #if android
-                addVirtualPad (UP_DOWN, A_B);
+                addVirtualPad(UP_DOWN, A_B);
                 #end
 
 		super.create();
