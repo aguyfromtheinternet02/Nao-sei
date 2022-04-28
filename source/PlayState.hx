4c109f36cc66cdc0f3a478e7487b3d63b2b3eca5
aguyfromtheinternet02 <102567443+aguyfromtheinternet02@users.noreply.github.com>
2022-04-28 10:12:12 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4560,7 +4560,6 @@ class PlayState extends MusicBeatState
 		}
 
 		// HARDCODING FOR MILF ZOOMS!
-			}
 		if (curSong.toLowerCase() == 'milf' && curBeat >= 168 && curBeat < 200 && camZooming && FlxG.camera.zoom < 1.35)
 		{
 			FlxG.camera.zoom += 0.015;
