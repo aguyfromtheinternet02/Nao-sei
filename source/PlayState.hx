6125c8467d3bce139433f5bc9bb3df163d1f22af
aguyfromtheinternet02 <102567443+aguyfromtheinternet02@users.noreply.github.com>
2022-04-30 20:48:03 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4863,7 +4863,7 @@ class PlayState extends MusicBeatState
 	{
 		super.beatHit();
 		
-			/*if (SONG.notes[Math.floor(curStep / 16)].mustHitSection && !qtCarelessFin){
+			if (SONG.notes[Math.floor(curStep / 16)].mustHitSection && !qtCarelessFin){
 				if(SONG.song.toLowerCase() == "cessation"){
 					if((curStep >= 640 && curStep <= 794) || (curStep >= 1040 && curStep <= 1199))
 					{
@@ -4876,7 +4876,7 @@ class PlayState extends MusicBeatState
 					dad.dance();
 			}
 
-		}*/
+		}
 
 		// Copy and pasted the milf code above for censory overload -Haz
 		if (curSong.toLowerCase() == 'censory-overload')
@@ -5043,7 +5043,7 @@ class PlayState extends MusicBeatState
 		{
 			if (!(SONG.notes[Math.floor(curStep / 16)].mustHitSection) && !dad.animation.curAnim.name.startsWith("sing"))
 			{
-				/*if(!qtIsBlueScreened && !qtCarelessFin)
+				if(!qtIsBlueScreened && !qtCarelessFin)
 					if(SONG.song.toLowerCase() == "cessation"){
 						if((curStep >= 640 && curStep <= 794) || (curStep >= 1040 && curStep <= 1199))
 						{
@@ -5055,7 +5055,7 @@ class PlayState extends MusicBeatState
 					else
 						dad.dance();
 			}
-		}*/
+		}
 
 		//Same as above, but for 404 variants.
 		if(qtIsBlueScreened)
