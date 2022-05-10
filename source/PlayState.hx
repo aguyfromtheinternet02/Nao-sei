e5eeb5d81d1e57eb5fd12806cda07edce88260bb
aguyfromtheinternet02 <102567443+aguyfromtheinternet02@users.noreply.github.com>
2022-05-10 20:01:10 -0300

    FUCK YOU

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4034,6 +4034,7 @@ class PlayState extends MusicBeatState
   }
   }
   }
+  }
 	private function popUpScore(strumtime:Float, note:Note):Void
 	{
 		var noteDiff:Float = Math.abs(strumtime - Conductor.songPosition);
