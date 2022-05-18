86b3e503c5817c425a6f8758ecdfb678d188421c
aguyfromtheinternet02 <102567443+aguyfromtheinternet02@users.noreply.github.com>
2022-05-18 16:17:53 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4260,6 +4260,7 @@ class PlayState extends MusicBeatState
   }
   }
   }
+  }
 	private function popUpScore(strumtime:Float, note:Note):Void
 	{
 		var noteDiff:Float = Math.abs(strumtime - Conductor.songPosition);
